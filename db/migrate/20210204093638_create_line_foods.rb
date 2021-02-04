579f320d31aa7c7ac93cdaf81da2ba322d312514
Kazuki Yamashiro <yamakazuki0721@gmail.com>
2021-02-04 18:51:38 +0900

    migrationファイルを作成

---
+++ db/migrate/20210204093638_create_line_foods.rb
@@ -0,0 +1,14 @@
+class CreateLineFoods < ActiveRecord::Migration[6.1]
+  def change
+    create_table :line_foods do |t|
+      t.references :food, null: false, foreign_key: true
+      t.references :restaurant, null: false, foreign_key: true
+      # orderが確定するまでlinefoodはorderを知り得ないので初期はnul: true
+      t.references :order, foreign_key: true
+      t.integer :count, null: false, default: 0
+      t.boolean :active, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
